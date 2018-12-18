183a5f1f44245f10b453e701f47ce9cac7889850
Darko <danielviana18@gmail.com>
2018-12-18 19:55:00 +0000

    passagem para o php

---
+++ DCW Website/informacoes.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Formulário de Solicitação de Informações</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main4.css">
+    </head>
+    <body>
+        <div class="cabecalho">
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li id="vhs"><a href="index.html"><h1>KGB- Alojamento Local</h1></a></li>
+                        <li><a href="index.html">Home</a></li><!--
+                     --><li><a href="index.html#901">Serviços</a></li><!--
+                     --><li><a href="index.html#902">Perfil</a></li><!--
+                     --><li><a href="index.html#903">Ofertas</a></li><!--
+                     --><li><a href="index.html#904">Informações</a></li><!--
+                     --><li><a href="login.html">Entrar</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <form action="servicos" method="post" class="form-servicos" >
+            <h2 class="form-titulo">Informações</h2>
+            <div class="input">Solicitação de Informações</div>
+            <input type="text" name="nome" placeholder="Nome">
+            <input type="text" name="email" placeholder="Email">
+            <textarea name="mensagem" placeholder="Escreva aqui a sua mensagem."></textarea>
+            <input type="button" value="Enviar" id="botao">
+        </form>
+        
+    </body>
+    </html>
---
+++ DCW Website/servicos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Formulário de Serviços</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main3.css">
+    </head>
+    <body>
+        <div class="cabecalho">
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li id="vhs"><a href="index.html"><h1>KGB- Alojamento Local</h1></a></li>
+                        <li><a href="index.html">Home</a></li><!--
+                     --><li><a href="index.html#901">Serviços</a></li><!--
+                     --><li><a href="index.html#902">Perfil</a></li><!--
+                     --><li><a href="index.html#903">Ofertas</a></li><!--
+                     --><li><a href="index.html#904">Informações</a></li><!--
+                     --><li><a href="login.html">Entrar</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <form action="servicos" method="post" class="form-servicos" >
+            <h2 class="form-titulo">Serviços</h2>
+            <div class="input" align="center">Solicitação de Serviços</div>
+            <input type="text" name="nome" placeholder="Nome">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="telemovel" placeholder="Telemóvel">
+            <div class="bloco" align="left">
+                <label for="exampleInputEmail1">Serviço:</label>
+                <select class="form-control">
+                    <option value="145">Check-in</option>
+                    <option value="146">Limpeza e Manutenção</option>
+                    <option value="147">Consultoria e Decoração</option>
+                </select>
+            </div>
+            <input type="button" value="Enviar" id="botao">
+        </form>
+        
+    </body>
+    </html>
