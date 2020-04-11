4e18654ba61f35597756b30788742eb5b0af94f0
Daniel Sá <danielviana18@gmail.com>
2020-04-11 18:34:47 +0100

    Database changes

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php 
-$server = "fdb26.awardspace.net";
-$user = "2942054_sistreserva";
-$password = "jose1989";
-$db = "2942054_sistreserva";
+$server = "remotemysql.com";
+$user = "SaCsSKrPaN";
+$password = "mbc3XKJ49w";
+$db = "SaCsSKrPaN";
 
 
 $conn = mysqli_connect($server, $user, $password, $db);
