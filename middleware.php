41bcc32f2b50cb2f97284d36adaae6948ec389d0
Daniel Sá <danielviana18@gmail.com>
2020-04-27 20:54:04 +0100

    Frontend: Choice's page done

---
+++ middleware.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/middleware.css">
+    <title>KGB</title>
+</head>
+<body>
+    <div class="container" align="center">
+        <div class="header" align="center">
+            <div class="headerMenu" align="center">
+                    <a href="#"><img src="./assets/home_logo.png" alt="logo" style="width: 76px; height: 76px; margin-top: 10px; padding: 0; "/></a>
+                    <div class="menu">
+                        <nav>
+                            <ul>
+                                <li><a href="#">Managers</a></li>
+                                <li><a href="#">Guests</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div class="headImg">
+                <img src="./assets/home_asset.jpg" alt="home_image" style="width: 1100px; height: 618.75; filter: blur(4px);
+                box-sizing: border-box;
+                box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                mix-blend-mode: luminosity;
+                border: 1px solid #000000;
+                max-width: 1100px;" align="center"/>
+                <button class="b1" id="b1" align="center"><a href="#">Managers</a></button>
+                <button class="b1" id="b2" align="center"><a href="#">Guests</a></button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
